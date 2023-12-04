52cb256ac90f34b6d834e50de2dcb186865adc00
abhijeeeth <112998926+abhijeeeth@users.noreply.github.com>
2023-12-04 17:08:21 +0530

    first commit

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
     <div className="mb-24 container mx-auto my-16 p-8 bg-black/50 text-white rounded-lg shadow-lg sm:mx-8">
       <h2 className="text-3xl font-bold mb-6">About Me</h2>
       <p className="text-lg mb-4">
-        Hi, I'm Abhijith Shaji, a passionate web developer with expertise in Next.js, React.js, Tailwind CSS, Git, and JavaScript.
+        Hi, I am Abhijith Shaji, a passionate web developer with expertise in Next.js, React.js, Tailwind CSS, Git, and JavaScript.
       </p>
       <p className="text-lg mb-4">
         I enjoy building web applications that are not only visually appealing but also deliver a great user experience. My focus is on creating clean, efficient, and maintainable code.
